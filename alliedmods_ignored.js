7846b902f5384a10fa554f62459b2ee52b53ad2e
Aoi Kagase <datumoujidai@gmail.com>
2020-07-27 13:48:50 +0900

    Create alliedmods_ignored.js

---
+++ alliedmods_ignored.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name		AlliedModders Remove Ignored UserPost
+// @include		*://forums.alliedmods.net/forumdisplay.php*
+// @include		*://forums.alliedmods.net/showthread.php*
+// @include		*://forums.alliedmods.net/showpost.php*
+// @version		0.2
+// @date		2020-07-13
+// @creator		Aoi.Kagase
+// @require		http://code.jquery.com/jquery-latest.min.js
+// @description	Remove all posts ignored users
+// ==/UserScript==
+
+var ignored = [];
+$.ajax({
+	type: 'GET',
+	dataType: 'text',
+	scriptCharset: 'UTF-8',
+	timeout: 10000,
+	cache: false,
+	url:'https://raw.githubusercontent.com/AoiKagase/AlliedModdersForumIgnoredRemover/master/denylist.json',
+}).done(function(data, status, xhr) {
+	ignored = JSON.parse(data);
+	$('td[id^=td_threadtitle_]').each(function() {
+		var name = $.trim($(this).find("div[class=smallfont]").find("span").text());
+		if ($.inArray(name,ignored) !== -1)
+		{
+			$(this).parent().hide();
+			console.log("Found " + name);
+		}
+	});
+	$('table[id^=post]' + 'table[class=tborder]').each(function() {
+		$(this).parent().parent().hide();
+	});
+});
+
